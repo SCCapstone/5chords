c92f2b6a572357919ccec36eb731373e8722cd57
zwaskins <askinsz@email.sc.edu>
2015-10-28 08:48:21 -0400

    Changed the background color to red.

--- app/src/main/java/com/example/drea/helloworld/MainActivity.java
+++ app/src/main/java/com/example/drea/helloworld/MainActivity.java
@@ -19,9 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     SoundPool mySound;
 
-    //////
     // major chords
-    //
     int[][] chords = {{0,4,7}, {1,5,8}, {2,6,9},          // C, C#, D
                       {3,7,10}, {4,8,11}, {5,9,12},       // Eb, E, F
                       {6,10,13}, {7,11,14}, {8,12,15},    // F#, G, Ab
@@ -37,9 +35,8 @@ public class MainActivity extends AppCompatActivity {
         mySound = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
         notes = new ArrayList<>();
 
-        //////
+
         // Loop through raw folder for notes
-        //
         for (Field f : R.raw.class.getFields()) {
             try {
                 if (f.getName().contains("note")) {
@@ -51,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // didnt do yet
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -58,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // didn't do yet
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -73,20 +72,17 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //////
-    // pick a random chord
-    //
+    /****************************************************************
+     * Picks a Random Chord and plays it
+     **/
     public void Rando(View v) {
-        //////
+
         // new random int from 0-11
-        // or use chords.length() in future
-        //
         Random rand = new Random();
         chord = chords[rand.nextInt(12)];
+        // or use chords.length() in future
 
-        //////
         // get notes in chord, play notes
-        //
         chordRoot = notes.get(chord[0]);
         chordThird = notes.get(chord[1]);
         chordFifth = notes.get(chord[2]);
@@ -96,36 +92,29 @@ public class MainActivity extends AppCompatActivity {
         mySound.play(chordFifth,1,1,1,0,1);
     }
 
-    //////
-    // check chord built against random chord
-    //
+    /****************************************************************
+     * check chord built against random chord
+     **/
     public void check(View v) {
-        //////
+
         // get the seek bars
-        //
         SeekBar root = (SeekBar) findViewById(R.id.root);
         SeekBar third = (SeekBar) findViewById(R.id.third);
         SeekBar fifth = (SeekBar) findViewById(R.id.fifth);
 
-        //////
         // get the note of each seek bar
-        //
         int thisRoot = notes.get(root.getProgress());
         int thisThird = notes.get(third.getProgress());
         int thisFifth = notes.get(fifth.getProgress());
 
-        //////
         // play the notes
-        //
         mySound.play(thisRoot,1,1,1,0,1);
         mySound.play(thisThird,1,1,1,0,1);
         mySound.play(thisFifth,1,1,1,0,1);
 
-        //////
         // get the labels
         // show correct answer on one label
         // show if the chords match on the other
-        //
         TextView chordLabel = (TextView) findViewById(R.id.chord);
         TextView answerLabel = (TextView) findViewById(R.id.answer);
 
@@ -138,9 +127,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //////
-    // replay the chord
-    //
+    /****************************************************************
+     * Replays the reandomly chosen chord
+     **/
     public void chordAgain(View v) {
         mySound.play(chordRoot,1,1,1,0,1);
         mySound.play(chordThird,1,1,1,0,1);
