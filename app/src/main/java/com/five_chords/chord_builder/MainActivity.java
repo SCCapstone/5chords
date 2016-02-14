6d31c25bb0661f3cdc37ecfa85c4d8291c3a98f2
zeagler <zeagler@email.sc.edu>
2016-02-13 20:25:05 -0500

    Add initial Midi (unincorporated) functionality.

--- app/src/main/java/com/five_chords/chord_builder/MainActivity.java
+++ app/src/main/java/com/five_chords/chord_builder/MainActivity.java
@@ -11,7 +11,6 @@ package com.five_chords.chord_builder;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -19,7 +18,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 
@@ -39,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements CheckOptionsFragm
     CheckOptionsFragment cof;
 
     int currentChordIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
