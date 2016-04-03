134cedc91e4538100b051e1535487022e7d901b8
Theodore Stone <tstone@email.sc.edu>
2016-04-03 17:38:23 -0400

    Added remaining documentation to the soundHandler class.

--- app/src/main/java/com/five_chords/chord_builder/soundHandler.java
+++ app/src/main/java/com/five_chords/chord_builder/soundHandler.java
@@ -8,36 +8,63 @@ import android.util.Log;
 import com.five_chords.chord_builder.com.five_chords.chord_builder.activity.MainActivity;
 
 /**
- * soundHandler.java
+ * Class to handle playing sounds. Sounds are played as MIDI files, which are created with one or more
+ * notes using the functions of this class.
  * @version 1.1
  * @date 3 April 2016
- * @author: Drea,Zach
+ * @author: Drea, Zach, Theodore
  **/
 public class soundHandler extends MainActivity
 {
     /** The tag for this class. */
     private static final String TAG = "soundHandler";
 
+    /** Constant representing full volume. */
     static final int FULL_VOLUME = 127;
+
+    /** Constant denoting that a note should be sustained. */
     static final int SUSTAIN_NOTE = 128;
+
+    /** Constant denoting that a note should be ended. */
     static final int RELEASE_NOTE = 0;
+
+    /** The offset in notes of the third octave. */
     static final int NOTE_OFFSET_OCTAVE3 = 60;
+
+    /** The offset in notes of the fourth octave. */
     static final int NOTE_OFFSET_OCTAVE4 = 72;
 
+    /** Constant id representing a Trumpet. */
     static final int TRUMPET = 57;
+
+    /** Constant id representing a Piano. */
     static final int PIANO = 2;
+
+    /** Constant id representing an organ. */
     static final int ORGAN = 16;
+
+    /** Constant id representing a Violin. */
     static final int VIOLIN = 41;
+
+    /** Constant id representing a Flute. */
     static final int FLUTE = 74;
 
+    /** Reference to the current instrument being used to create sounds. */
     static int instrument = PIANO;
 
-    MediaPlayer mediaPlayer;
-    soundHandlerMidi midi;
-    String midiFile;
+    /** The MediaPlayer to use to play the MIDI files. */
+    private MediaPlayer mediaPlayer;
+
+    /** The soundHandlerMidi to use to create the MIDI files. */
+    private soundHandlerMidi midi;
+
+    /** The name of the current MIDI file. */
+    private String midiFile;
 
     /**
-     * Static class.
+     * Creates a new SoundHandler.
+     * @param main The current Activity
+     * @param midiFileName The name of the MIDI file to use
      */
     public soundHandler(Activity main, String midiFileName)
     {
@@ -46,37 +73,66 @@ public class soundHandler extends MainActivity
     }
 
     /**
-     * Stop the media player
+     * Called to change the global instrument being used to play sounds.
+     * @param i The index of the new Instrument to use
+     */
+    public static void switchInstrument(int i)
+    {
+        if(i == 0) instrument = TRUMPET;
+        if(i == 1) instrument = PIANO;
+        if(i == 2) instrument = ORGAN;
+        if(i == 3) instrument = VIOLIN;
+        if(i == 4) instrument = FLUTE;
+    }
+
+    /**
+     * Gets the index of the current global instrument being used to play sounds.
+     * @return The index of the current global instrument being used to play sounds
+     */
+    public static int getInstrument()
+    {
+        return instrument;
+    }
+
+    /**
+     * Stops the media player for this soundHandler.
      */
     public void stopSound() {
         Log.d(TAG, "Stop Sound");
         try {
             mediaPlayer.release();
         } catch (Exception e) {
-
+            /* Ignore */
         }
     }
 
-    /****************************************************************
-     * Add a note to the MIDI file
-     **/
+    /**
+     * Adds a note to the MIDI file.
+     * @param note The note to add
+     * @param channel The channel to which to add the note
+     */
     public void addNote(Note note, int channel)
     {
         addNote(note.index, 8192 + (int)(4096 * note.distanceToIndex), channel);
     }
 
-    /****************************************************************
-     * Add a note to the MIDI file
-     **/
+    /**
+     * Adds a note to the MIDI file.
+     * @param note The note to add, represented by an integer
+     * @param pitch The pitch of the note to add, in the range [0, 16383]
+     *              (corresponding to an offset in the range [-1, 1] from the integer note index)
+     * @param channel The channel to which to add the note
+     */
     public void addNote(int note, int pitch, int channel)
     {
-        // TODO: use the sliders to change pitch of note
-        //       replace 8192 (centered) with the new pitch
+        // Calculate the two byte form of the pitch
         int mostSignificantbits = (pitch >> 7) & 0x7F;
         int leastSignificantbits = pitch & 0x7F;
 
+        // The default note offset
         int midiOffSet = NOTE_OFFSET_OCTAVE3;
 
+        // If needed, change the note offset to compensate for the unique sound of the current instrument
         switch (instrument) {
             case TRUMPET: midiOffSet = NOTE_OFFSET_OCTAVE3;
                           break;
@@ -90,25 +146,36 @@ public class soundHandler extends MainActivity
                           break;
         }
 
+        // Set the channel, instrument, and pitch of the note in the MIDI file
         midi.setChannel(channel);
         midi.progChange(instrument);
         midi.bendPitch(mostSignificantbits, leastSignificantbits);
+
+        // Set the volume and duration of the note in the MIDI file
         midi.noteOn(RELEASE_NOTE, note + midiOffSet, FULL_VOLUME);
         midi.noteOff(SUSTAIN_NOTE, note + midiOffSet);
         midi.commitTrack();
     }
 
-    /****************************************************************
-     * Plays a chord
-     **/
+    /**
+     * Plays a chord represented by an array of notes.
+     * @param activity The current Activity
+     * @param chord The chord to play
+     * @param numNotes The number of notes in the chord to play
+     */
     public void playChord(Activity activity, Note[] chord, int numNotes)
     {
+        // Stop the current sound if needed
         stopSound();
 
+        // Create a new MIDI with the correct number of channels
         midi.newMidi(numNotes, 1);
+
+        // Add the notes to the MIDI
         for (int i = 0; i < numNotes; i++)
             addNote(chord[i], i);
 
+        // Write and play the MIDI file
         try
         {
             midi.writeToFile(midiFile);
@@ -118,20 +185,25 @@ public class soundHandler extends MainActivity
         }
         catch (Exception e)
         { /* Ignored */ }
-
-        Log.d(TAG, "Done Playing Chord");
     }
 
-    /****************************************************************
-     * Plays a note
-     **/
+    /**
+     * Plays a single note.
+     * @param activity The current Activity
+     * @param note The note to play
+     */
     public void playNote(Activity activity, Note note)
     {
+        // Stop the current sound if needed
         stopSound();
 
+        // Create a new MIDI with the correct number of channels
         midi.newMidi(1, 0);
+
+        // Add the note to the MIDI
         addNote(note, 1);
 
+        // Write and play the MIDI file
         try
         {
             midi.writeToFile(midiFile);
@@ -141,20 +213,5 @@ public class soundHandler extends MainActivity
         }
         catch (Exception e)
         { /* Ignored */ }
-
-        Log.d(TAG, "Done Playing Note");
-    }
-
-    public static void switchInstrument(int i)
-    {
-        if(i == 0) instrument = TRUMPET;
-        if(i == 1) instrument = PIANO;
-        if(i == 2) instrument = ORGAN;
-        if(i == 3) instrument = VIOLIN;
-        if(i == 4) instrument = FLUTE;
-    }
-
-    public static int getInstrument() {
-        return instrument;
     }
 }
