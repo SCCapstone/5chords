929c4028d25f35ccd3f4039174080c1e80ece56a
MissingPiece <khyun12@gmail.com>
2016-04-03 21:33:10 -0400

    Merge branch 'master' of C:\Users\Kevin\AndroidStudioProjects\5chords with conflicts.

--- app/src/main/java/com/five_chords/chord_builder/com/five_chords/chord_builder/activity/demo.java
+++ app/src/main/java/com/five_chords/chord_builder/com/five_chords/chord_builder/activity/demo.java
@@ -7,6 +7,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.five_chords.chord_builder.R;
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.targets.Target;
+import com.github.amlcurran.showcaseview.targets.ActionViewTarget;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
 
 /**
  * The startup demo Activity.
@@ -14,8 +18,11 @@ import com.five_chords.chord_builder.R;
  * @date 06 November 2015
  * @author Drea,Steven,Zach,Kevin,Bo
  */
-public class demo extends AppCompatActivity
+public class demo extends AppCompatActivity implements View.OnClickListener
 {
+    private ShowcaseView showcaseView;
+    private int count = 0;
+    private Target t0, t1, t2,t3,t4,t5,t6,t7;
     /**
      * Activity Create and checks first launch
      * If first launch, go to Tutorial, otherwise
@@ -24,11 +31,11 @@ public class demo extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+       super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
 
         final ImageView imageView = (ImageView)findViewById(R.id.imageView);
-
+/*
         Button next_button = (Button)findViewById(R.id.next_button);
 
         next_button.setOnClickListener(new View.OnClickListener() {
@@ -38,9 +45,75 @@ public class demo extends AppCompatActivity
                 imageView.setImageResource(R.drawable.test2);
                 v.setEnabled(false);
             }
-        });
-    }
+        }); */
 
+        t0 = new ViewTarget(R.id.button, this);
+        t1 = new ViewTarget(R.id.button2, this);
+        t2 = new ViewTarget(R.id.button3, this);
+        t3 = new ViewTarget(R.id.button4, this);
+        t4 = new ViewTarget(R.id.button5, this);
+        t5 = new ViewTarget(R.id.button6, this);
+        t6 = new ViewTarget(R.id.button7, this);
+        t7 = new ViewTarget(R.id.button8, this);
+
+
+        showcaseView = new ShowcaseView.Builder(this)
+                .setTarget(Target.NONE)
+                .setOnClickListener(this)
+                .setContentTitle("Tutorial!")
+                .setContentText("Welcome to Chord Builder! We will walk you through a quick demo to get you used to this tool!")
+                .build();
+        showcaseView.setButtonText("Next");
+
+    }
+    public void onClick(View v){
+        switch(count){
+            case 0:
+                showcaseView.setShowcase(t0,true);
+                showcaseView.setContentTitle("Play");
+                showcaseView.setContentText("Hold down the Play button to hear the selected chord");
+                break;
+            case 1:
+                showcaseView.setShowcase(t1,true);
+                showcaseView.setContentTitle("Chord Select");
+                showcaseView.setContentText("Select the Chord that are Available to you here, and see your level next to it as well.");
+                break;
+            case 2:
+                showcaseView.setShowcase(t2,true);
+                showcaseView.setContentTitle("Menu");
+                showcaseView.setContentText("Swipe to the Right here to gain access to more options and more information about the Chord Builder. You can also check your history and adjust the difficulty in the menu.");
+                break;
+            case 3:
+                showcaseView.setShowcase(t3,true);
+                showcaseView.setContentTitle("Slider");
+                showcaseView.setContentText("Hold and drag the Slider button to change the notes of the chord you are creating. You can also hold the slider down to listen to the note selected.");
+                break;
+            case 4:
+                showcaseView.setShowcase(t5,true);
+                showcaseView.setContentTitle("Preview");
+                showcaseView.setContentText("Hold the Preview Button to hear the chord you have currently selected with the sliders.");
+                break;
+            case 5:
+                showcaseView.setShowcase(t4,true);
+                showcaseView.setContentTitle("Check Your Work!");
+                showcaseView.setContentText("Once the selected and the desired chords sound the same. Check your work by clicking this button here! ");
+                break;
+            case 6:
+                showcaseView.setShowcase(t6,true);
+                showcaseView.setContentTitle("Randomize It!");
+                showcaseView.setContentText("When you think you're ready for the challenge, press the Shuffle button for a random chord for you to try!");
+                break;
+            case 7:
+                showcaseView.setShowcase(t7,true);
+                showcaseView.setContentTitle("Enjoy!");
+                showcaseView.setContentText("Thank you for trying out our appplication and feel free to contact us if you have any questions or comments. Thank you!");
+                break;
+
+            case 8:
+                finish();
+        }
+        count++;
+    }
     /**
      * Called to go the MainActivity.
      * @param view The calling View
